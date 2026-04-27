d63131dd68c6d218d385c159f0552dec19f09d79
yang <jason.chi@gyro.com.tw>
2026-04-27 19:06:36 +0800

    update

--- assets/js/articles.data.js
+++ assets/js/articles.data.js
@@ -11,6 +11,19 @@ const articles = [
     subTags: ["日本酒"],
     relatedProductIds: [31, 30, 39, 44]
   },
+  {
+    id: 2,
+    slug: "try-spring-sake",
+    title: "跟著季節呼吸｜清酒，品味四季最溫柔的載體",
+    excerpt: "一起尋覓屬於春季的風味：新酒．初榨．生霞酒！",
+    cover: "/assets/images/blog/部落文封面.webp",
+    date: "2026/04/27",
+    author: "酉時喝酒",
+    mainTags: ["酉時圖書館"],
+    subTags: ["日本酒"],
+    relatedProductIds: [80, 32, 44, 63, 58]
+  }
+
 ];
 // 強制掛載到全域 window 物件以確保跨腳本讀取
 if (typeof window !== 'undefined') {
